0aaa968ef87001e2d06af0103434b07b75cfe651
MelvinMuuo <muuomelvin73@gmail.com>
2019-06-19 19:13:44 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/myrestaurantz/MainActivityInstrumentationTest.java
@@ -0,0 +1,4 @@
+package com.example.myrestaurantz;
+
+public class MainActivityInstrumentationTest {
+}
---
+++ app/src/main/java/com/example/myrestaurantz/adapters/RestaurantListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.myrestaurantz.adapters;
+
+public class RestaurantListAdapter {
+}
